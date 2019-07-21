808dd3d3a958757e57e64e9f6f86425abec308d9
Kasper B. Graversen <kbilsted@users.noreply.github.com>
2019-07-22 00:58:29 +0200

    Extract options parser

--- src/CommodoreBasicReformatter/Program.cs
+++ src/CommodoreBasicReformatter/Program.cs
@@ -10,29 +10,51 @@ namespace CommodoreBasicReformatter
         {
             if (args.Length != 2 && args.Length != 3)
             {
-                Console.WriteLine();
-                Console.WriteLine("NAME");
-                Console.WriteLine("    CommodoreBasicReformatter");
-                Console.WriteLine("SYNTAX");
-                Console.WriteLine("    CommodoreBasicReformatter [--split-lines] <infile> <outfile>");
-                Console.WriteLine("REMARKS");
-                Console.WriteLine("    (c) 2019 Kasper B. Graversen and Jacob Dahl Pind");
-                Console.WriteLine();
+                PrintHelp();
                 return;
             }
 
-            int i = 0;
-            bool splitLines = false;
-            if (args[i] == "--split-lines")
-            {
-                splitLines = true;
-                i++;
-            }
-            var input = args[i++];
-            var output = args[i++];
+            var options = Options.Parse(args);
 
-            var result = new Reformatter(new StmtsSplitter()).Reformat(File.ReadAllText(input), splitLines);
-            File.WriteAllBytes(output, Encoding.ASCII.GetBytes(result));
+            var result = new Reformatter(new Grammar(), new StmtsSplitter())
+                .Reformat(File.ReadAllText(options.Input), options.SplitLines);
+
+            File.WriteAllBytes(options.Output, Encoding.ASCII.GetBytes(result));
+        }
+
+        class Options
+        {
+            public string Input, Output;
+            public bool SplitLines;
+
+            public static Options Parse(string[] args)
+            {
+                var options = new Options();
+                int i = 0;
+
+                if (args[i] == "--split-lines")
+                {
+                    options.SplitLines = true;
+                    i++;
+                }
+
+                options.Input = args[i++];
+                options.Output = args[i++];
+
+                return options;
+            }
+        }
+
+        private static void PrintHelp()
+        {
+            Console.WriteLine();
+            Console.WriteLine("NAME");
+            Console.WriteLine("    CommodoreBasicReformatter");
+            Console.WriteLine("SYNTAX");
+            Console.WriteLine("    CommodoreBasicReformatter [--split-lines] <infile> <outfile>");
+            Console.WriteLine("REMARKS");
+            Console.WriteLine("    (c) 2019 Kasper B. Graversen and Jacob Dahl Pind");
+            Console.WriteLine();
         }
     }
 }
